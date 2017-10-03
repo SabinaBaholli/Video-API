4f931f63d1f64de1faae4e80bc28a43a24bf7edc
Sabina Baholli <sabinabaholli@gmail.com>
2017-10-04 00:29:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Crossover Video Portal backend
+This is the backend API code that needs to be consumed by front-end applications.
+# Video-API
---
+++ config.js
@@ -0,0 +1,6 @@
+var configs = {};
+configs.applicationPort = 3000;
+configs.dbName = 'mongodb';
+configs.dbHost = 'localhost';
+
+module.exports = configs;
